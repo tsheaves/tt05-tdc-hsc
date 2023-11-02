ad0e9621770c03682236e3109b1abb9a085a04d2
Tyler Sheaves <tsheaves@ucdavis.edu>
2023-11-02 10:30:48 -0700

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -10,10 +10,10 @@ module sky130_fd_sc_hd__buf_1 (
     input  A (* keep *);
 
     // Voltage supply signals
-    supply1 VPWR (* keep *);
-    supply0 VGND (* keep *);
-    supply1 VPB (* keep *);
-    supply0 VNB (* keep *);
+    supply1 VPWR;
+    supply0 VGND;
+    supply1 VPB;
+    supply0 VNB;
 
 endmodule
 
@@ -31,6 +31,6 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     assign uio_out [7:0] = 8'b0;
     assign uio_oe [7:0]  = 8'hFF;
     
-    sky130_fd_sc_hd__buf_1 delay0(.X(uo_out[0]),  .A(ui_in[0])) (* keep *);
+    sky130_fd_sc_hd__buf_1 delay0(.X(uo_out[0]),  .A(ui_in[0]));
 
 endmodule
