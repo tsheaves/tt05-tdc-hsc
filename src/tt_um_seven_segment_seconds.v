7466774742ae0f8025903c673860d1932b08c41e
Tyler Sheaves <tsheaves@ucdavis.edu>
2023-11-02 13:41:07 -0700

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -17,10 +17,33 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
     
     (* keep *) wire buff_in;
     (* keep *) wire buff_out;
+    (* keep *) wire buff_out_d;
+    (* keep *) wire buff_out_q;
+    (* keep *) wire buff_in_q;
 
-    assign uo_out[0] = buff_out;
-    assign ui_in[0] = buff_in;
+    assign uo_out[0] = buff_out_q;
+    assign buff_in = ui_in[0];
 
-    sky130_fd_sc_hd__buf_1 delay0(.X(buff_out),  .A(buff_in));
+    sky130_fd_sc_hd__dfbbp_1 df0(
+        .Q(buff_in_q),
+        .Q_N(),
+        .D(buff_in),
+        .CLK(clk),
+        .SET_B(1'b1),
+        .RESET_B(rst_n)
+    );
+    
+    sky130_fd_sc_hd__buf_1 buff0(
+        .X(buff_out_d),  
+        .A(buff_in_q));
+    
+    sky130_fd_sc_hd__dfbbp_1 df1(
+        .Q(buff_out),
+        .Q_N(),
+        .D(buff_out_d),
+        .CLK(clk),
+        .SET_B(1'b1),
+        .RESET_B(rst_n)
+    );
 
 endmodule
